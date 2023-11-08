3f42b766c2718ca902313c798027c6e39a7f594b
nathanclairmonte <nathanclairmonte@outlook.com>
2023-11-08 11:48:13 -0500

    added connectMongo function

---
+++ src/utils/connectMongo.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+// function to connect to the MongoDB client
+async function connectMongo() {
+    try {
+        // Connect the client to the server
+        const { connection } = await mongoose.connect(process.env.MONGO_URI);
+
+        // check ready state and return promise
+        if (connection.readyState === 1) {
+            return Promise.resolve(true);
+        }
+    } catch (error) {
+        return Promise.reject(error);
+    }
+}
+
+export default connectMongo;

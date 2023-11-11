afe8c2bf833fa870ac922dba665523088a94bc14
nathanclairmonte <nathanclairmonte@outlook.com>
2023-11-11 09:00:52 -0500

    BottomBar back to placeholder

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,27 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import { AiFillHome, AiOutlineInbox, AiFillProfile, AiOutlineSearch } from "react-icons/ai";
+import { FaMagnifyingGlass, FaHouse, FaInbox, FaUser } from "react-icons/fa6";
 
 const BottomBar = () => {
     return (
         <footer className="fixed bottom-0 w-full">
-            <div className="flex items-center justify-center h-16 rounded-t-lg bg-white shadow-[0px_0px_5px_1px] space-x-4">
-                <Link href="/explore" className="text-blue-500 flex items-center space-x-2">
-                    <AiOutlineSearch />
-                    <span>Explore</span>
-                </Link>
-                <Link href="/sublets" className="text-blue-500 flex items-center space-x-2">
-                    <AiFillHome />
-                    <span>Sublets</span>
-                </Link>
-                <Link href="/inbox" className="text-blue-500 flex items-center space-x-2">
-                    <AiOutlineInbox />
-                    <span>Inbox</span>
-                </Link>
-                <Link href="/profile" className="text-blue-500 flex items-center space-x-2">
-                    <AiFillProfile />
-                    <span>Profile</span>
-                </Link>
+            <div className="flex items-center justify-around py-6 rounded-t-lg bg-white shadow-[0px_0px_5px_1px] space-x-4">
+                Lorem Ipsum
             </div>
         </footer>
     );

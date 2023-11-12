e69fa785164976f23484d7760f96c79066b80f46
nathanclairmonte <nathanclairmonte@outlook.com>
2023-11-12 16:17:49 -0500

    wrapped app in AuthProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,12 @@ import "@/styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "@/styles/CarouselElement.css";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <AuthProvider>
+            <Component {...pageProps} />
+        </AuthProvider>
+    );
 }

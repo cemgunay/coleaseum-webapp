a196867f6489e39291f1b63dc75bb8690e4cc7eb
nathanclairmonte <nathanclairmonte@outlook.com>
2023-11-14 21:55:33 -0500

    added title + cn() to AuthInput component

--- src/components/AuthInput.js
+++ src/components/AuthInput.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Input from "@/components/Input";
+import { cn } from "@/utils/utils";
 
 // input component to be used for auth forms etc.
 // provides functionality for error messages and error styling
-const AuthInput = ({ error, touched, ...props }) => {
+const AuthInput = ({ title = "test", error, touched, className, ...props }) => {
     return (
-        <div className="flex flex-col">
+        <div className={cn("flex flex-col", className)}>
+            <label className="text-base font-medium text-slate-900 mb-1 ml-0">{title}</label>
             <Input className={touched && error ? "border-red-500" : ""} {...props} />
             {touched && error && <p className="text-sm ml-3 mt-1 text-red-500">{error}</p>}
         </div>

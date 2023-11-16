c2b201422dd437c6bb937266abc5287127e3dd62
nathanclairmonte <nathanclairmonte@outlook.com>
2023-11-16 17:43:57 -0500

    added Toaster component to DOM tree

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "@/styles/CarouselElement.css";
 import { AuthProvider } from "@/context/AuthContext";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function App({ Component, pageProps }) {
     return (
         <AuthProvider>
             <Component {...pageProps} />
+            <Toaster />
         </AuthProvider>
     );
 }

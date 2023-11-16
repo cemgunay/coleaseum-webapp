3dacc2c5ab1add470a5f2993d8c74f31244cd7bc
nathanclairmonte <nathanclairmonte@outlook.com>
2023-11-15 20:10:01 -0500

    added fn to fetch remaining user data if logged in

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useAuth } from "@/hooks/useAuth";
 import Skeleton from "@/components/Skeleton";
 import BottomNav from "@/components/BottomNav";
+import { Button as ShadButton } from "@/components/ui/button";
+import Button from "@/components/Button";
 
 const profile = () => {
     // get user object from context
-    const { user, loading } = useAuth();
+    // will ONLY contain ID. this is for security reasons
+    // we will use this ID to fetch the rest of their info from the db
+    const { user: contextUser, loading, signOut } = useAuth();
+    console.log(contextUser);
+
+    // state for user object that will be fetched from db
+    const [user, setUser] = useState(null);
+
+    // fetch user data if user is logged in (i.e. contextUser is not null)
+    useEffect(() => {
+        // do nothing if user is not logged in
+        if (!contextUser) return;
+
+        // fetch user
+        const fetchUser = async () => {
+            const response = await fetch(`/api/users/${contextUser?.id}`);
+            if (!response.ok) {
+                throw new Error("Failed to fetch user :(");
+            }
+            const data = await response.json();
+            setUser(data);
+        };
+        fetchUser();
+    }, [contextUser]);
 
     // loading component
     const Loading = () => {
@@ -41,11 +66,25 @@ const profile = () => {
 
     return (
         <>
-            {user ? (
-                <div>
-                    <h1>Welcome, {user.firstName}!</h1>
-                    <p>Email: {user.email}</p>
-                    {/* gonna add more user info here and style properly */}
+            {contextUser ? (
+                <div className="flex flex-col items-start justify-start min-h-screen gap-5 mx-8 pt-10">
+                    <h1>Welcome, {user?.firstName}!</h1>
+                    <p>Email: {user?.email}</p>
+                    <ShadButton
+                        variant="outline"
+                        size="lg"
+                        className="font-normal text-base text-slate-600"
+                        // onClick={() => alert("log out")}
+                        onClick={signOut}
+                    >
+                        Sign Out
+                    </ShadButton>
+                    {/* <Button
+                        className="border border-slate-300 bg-white text-slate-500"
+                        onClick={() => alert("log out")}
+                    >
+                        Log out
+                    </Button> */}
                 </div>
             ) : (
                 <div className="flex flex-col items-start justify-start min-h-screen gap-5 mx-8 pt-10">

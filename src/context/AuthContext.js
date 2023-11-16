2bcb09ffc61b84e80e0b6364541312b2d6606933
nathanclairmonte <nathanclairmonte@outlook.com>
2023-11-15 20:08:49 -0500

    added saveUser function to AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,7 +6,7 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true); // helpful to update the UI accordingly
 
-    // check for user in local storage
+    // check for user in local storage on load
     useEffect(() => {
         const loggedInUser = localStorage.getItem("user");
         if (loggedInUser) {
@@ -15,8 +15,17 @@ export const AuthProvider = ({ children }) => {
         setLoading(false);
     }, []);
 
-    // logout function
-    const logout = () => {
+    // function to save user to local storage and context
+    const saveUser = (user) => {
+        // save user to local storage
+        localStorage.setItem("user", JSON.stringify(user));
+
+        // save user to context
+        setUser(user);
+    };
+
+    // sign out function
+    const signOut = () => {
         // Remove user from local storage
         localStorage.removeItem("user");
 
@@ -25,7 +34,7 @@ export const AuthProvider = ({ children }) => {
     };
 
     return (
-        <AuthContext.Provider value={{ user, setUser, loading, logout }}>
+        <AuthContext.Provider value={{ user, saveUser, loading, signOut }}>
             {children}
         </AuthContext.Provider>
     );

8a5741af5581ad93eb6fda880a5fa0c121e31400
nathanclairmonte <nathanclairmonte@outlook.com>
2023-11-18 12:54:03 -0500

    added API route to get all requests for a user

---
+++ src/pages/api/requests/[userId].js
@@ -0,0 +1,32 @@
+import connectMongo from "@/utils/connectMongo";
+import Request from "@/models/Request";
+
+// get all requests for a given user
+export default async function handler(req, res) {
+    // connect to DB
+    await connectMongo();
+
+    switch (req.method) {
+        case "GET":
+            try {
+                // get user ID from query (will be passed by dynamic endpoint functionality)
+                const { userId } = req.query;
+
+                // get all requests
+                const requests = await Request.find({ subTenantId: userId })
+                    .populate("tenantDocuments")
+                    .populate("subtenantDocuments");
+
+                // return
+                res.status(200).json(requests);
+            } catch (error) {
+                res.status(500).json({ error: `APIError: Failed to get requests.\n${error}` });
+            }
+        // case "POST":
+        //     // for later if we want to add filters
+        default:
+            // res.setHeader("Allow", ["GET", "POST"]);
+            res.setHeader("Allow", ["GET"]);
+            return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}

60c503f424e38665bde5c80af550557cf312b61d
nathanclairmonte <nathanclairmonte@outlook.com>
2023-12-15 11:49:45 -0500

    added API route to get all expired Ls for user

---
+++ src/pages/api/listings/expired/[userId].js
@@ -0,0 +1,38 @@
+import connectMongo from "@/utils/connectMongo";
+import mongoose from "mongoose";
+import Listing from "@/models/Listing";
+
+// get all expired listings for a user
+export default async function handler(req, res) {
+    // switch based on request method
+    switch (req.method) {
+        case "GET":
+            // get user ID from query (will be passed by dynamic endpoint functionality)
+            const { userId } = req.query;
+
+            // check if the userId is a valid Mongo ObjectId
+            if (!mongoose.Types.ObjectId.isValid(userId)) {
+                return res.status(404).json({ error: "User ID not valid" });
+            }
+
+            try {
+                // connect to DB
+                await connectMongo();
+
+                // get all expired listings for the user
+                const today = new Date();
+                today.setHours(0, 0, 0, 0);
+                const listings = await Listing.find({ userId, expiryDate: { $lt: today } });
+
+                // return the listings with 200 status code
+                res.status(200).json(listings);
+            } catch (error) {
+                console.error(`Failed to retrieve listings:\n`, error);
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+        default:
+            // got some method other than GET
+            res.setHeader("Allow", ["GET"]);
+            res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}

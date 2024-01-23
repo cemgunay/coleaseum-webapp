8c5f65f8989e39497dbe2fc6c88dbe7c1264e995
nathanclairmonte <nathanclairmonte@outlook.com>
2024-01-23 17:31:42 -0500

    update request create endpoint

--- src/pages/api/requests/create.js
+++ src/pages/api/requests/create.js
@@ -1,36 +1,45 @@
 import connectMongo from "@/utils/connectMongo";
 import Request from "@/models/Request";
-
 import pusher from "../../../utils/pusher";
 
-// Mock function to simulate number of bids
-// Returns number of bids
-const numberOfBidsMock = async (listingId) => {
-    // Mock logic to check number of bids
-    // Replace this with real database logic later
-    const numberOfBids = 5
-    return numberOfBids;
-  };
-
-//mock create api - add actual update stuff later
+// create new request
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { listingId } = req.body;
+    switch (req.method) {
+        case "POST":
+            try {
+                // connect to DB
+                await connectMongo();
 
-    try {
-      // Trigger a Pusher event
-      console.log('triggered create')
-      pusher.trigger("bids-channel", "bid-created", {
-        listingId: listingId,
-      });
+                // get listing/user/price info from req body
+                const { listingId, tenantId, subTenantId, price } = req.body;
+                console.log(req.body);
 
-      res
-        .status(200)
-        .json({ message: "Bid created successfully" });
-    } catch (error) {
-      res.status(500).json({ message: "Error creatinng bid" });
+                // create new request
+                const newRequest = new Request({
+                    listingId,
+                    tenantId,
+                    subTenantId,
+                    price,
+                    status: "pending",
+                });
+
+                await newRequest.save();
+
+                // trigger a pusher event
+                console.log("triggered create");
+                pusher.trigger("requests-channel", "request-created", {
+                    listingId: listingId,
+                });
+
+                // send back newly created request
+                res.status(201).json(newRequest);
+            } catch (error) {
+                console.error("Failed to create new request.\n", error);
+                res.status(500).json({ error: "Error creating new request." });
+            }
+            break;
+        default:
+            res.setHeader("Allow", ["POST"]);
+            return res.status(405).end(`Method ${req.method} Not Allowed`);
     }
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
-  }
 }

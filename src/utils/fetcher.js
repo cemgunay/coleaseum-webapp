bfd9a0517a0fa5e60eb4563b609623647bca7763
Cem Gunay <cgunay@uwaterloo.ca>
2024-03-20 14:26:17 -0400

    fixed syntax in fetcher

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -3,17 +3,22 @@ const fetcher = async (url, componentName) => {
 
     try {
         const response = await fetch(url);
-    // If the status code is not in the range 200-299,
-    // we still try to parse and throw it.
-    if (!res.ok) {
-        const error = new Error("An error occurred while fetching the data.");
-        // Attach extra info to the error object.
-        error.info = await res.json();
-        error.status = res.status;
-        throw error;
-    }
+        // If the status code is not in the range 200-299,
+        // we still try to parse and throw it.
+        if (!response.ok) {
+            const error = new Error("An error occurred while fetching the data.");
+            // Attach extra info to the error object.
+            error.info = await response.json();
+            error.status = response.status;
+            throw error;
+        }
 
-    return res.json();
+        return await response.json();
+    } catch (error) {
+        // Log the error or handle it as needed
+        console.error("Fetch error:", error);
+        throw error; // Rethrow if you want calling code to handle it as well.
+    }
 };
 
 export default fetcher;
